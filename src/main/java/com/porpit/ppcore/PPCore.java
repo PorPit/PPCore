faed6cd956af5c7d3564d990752511f7376a4a84
PorPit <692066768@qq.com>
2018-11-22 00:47:50 +0800

    modify mod Version

--- src/main/java/com/porpit/ppcore/PPCore.java
+++ src/main/java/com/porpit/ppcore/PPCore.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class PPCore {
     public static final String MODID = "ppcore";
     public static final String NAME = "PPCore";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     @Mod.Instance(PPCore.MODID)
     public static PPCore instance;
 
